b65f3450f9dc78129d98b962bbd1d5fe3dcfde22
George <lets.devel0pme@gmail.com>
2024-12-03 18:26:45 -0300

    logo, banner and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@
 import './App.css'
 import Designs from './Components/Designs/Designs'
 import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
 import { BrowserRouter,  Routes, Route, Navigate } from 'react-router-dom';
+import Banner from './Components/Banner/Banner';
+import Logo from './Components/Logo/Logo';
 function App() {
 
   return (
     <>
     <BrowserRouter>
+      <Banner />
+      <Logo />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='*' element={<Navigate to="/" />} />
         <Route path='/Designs' element={<Designs />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
     </>
   )
---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import Logo2 from '../../assets/logo-2.png'
+const Logo = () => {
+
+    return (
+
+        <div className="fixed top-0 left-0 flex">
+            <img className="h-auto w-36 m-2" src={Logo2} alt="Logo" />
+        </div>
+    )
+}
+
+export default Logo
