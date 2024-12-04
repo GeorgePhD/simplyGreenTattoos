937b2a7304c52d632d01c7ee2c6cce9db1f6fc91
George <lets.devel0pme@gmail.com>
2024-12-04 17:52:10 -0300

    banner modified before file from desktop

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
                     <span className="text-black">
                         - Oferta por tiempo limitado!
                     </span>
-                    <span title="" className="inline-flex items-center justify-center text-sm font-bold text-green-500 transition-all ml-4 duration-200 rounded-md hover:text-red-700" role="button">
+                    <span title="" className="inline-flex items-center justify-center text-sm font-bold text-green-500 transition-all ml-4 duration-200 rounded-md hover:text-green-700" role="button">
                         Hablémos<svg className="w-4 h-4 ml-1" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
                     </span>
                     </p>
