62c41d28c53a091a3831d6516418236326b5546c
George <lets.devel0pme@gmail.com>
2024-12-06 17:42:39 -0300

    update: menu added to the middle right side, transitions added

--- src/Components/HomeBtn/HomeBtn.jsx
+++ src/Components/HomeBtn/HomeBtn.jsx
@@ -1,35 +1,46 @@
-import { Link } from 'react-router-dom'
-const HomeBtn = () => {
+import { Link } from 'react-router-dom';
 
+const HomeBtn = () => {
     return (
-        <>
-        <Link to="/Home">
-            <button className="fixed top-[1%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">Ir a inicio
-            </button>
-        </Link>
+        <div className="fixed z-50 top-[50%] right-[-90px] sm:right-[-120px] translate-y-[-50%] flex flex-col gap-4 transition-all duration-500 hover:right-0">
+            <Link to="#">
+                <button className="relative right-[10px] sm:right-[25px] w-[130px]  font-semibold text-white bg-green-600 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    menu
+                </button>
+            </Link>
+            <Link to="/Home">
+                <button className="w-[130px]  font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    Ir a inicio
+                </button>
+            </Link>
             <Link to="/Designs">
-                <button className="fixed top-[35%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">Diseños
+                <button className="w-[130px] font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    Diseños
                 </button>
             </Link>
             <Link to="/Demos">
-                <button className="fixed top-[40%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">Demos
+                <button className="w-[130px] font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    Demos
                 </button>
             </Link>
             <Link to="/ImplementosDeTrabajo">
-                <button className="fixed top-[45%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">Implepentos
+                <button className="w-[130px] font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    Implementos
                 </button>
             </Link>
             <Link to="/Privacidad">
-                <button className="fixed top-[50%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">
+                <button className="w-[130px] font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
                     Privacidad
                 </button>
             </Link>
             <Link to="/Cookies">
-                <button className="fixed top-[55%] right-[1%] flex items-center justify-center w-50 h-auto font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 mt-4 w-[130px]">Cookies
+                <button className="w-[130px] font-semibold text-white bg-green-500 border border-green-500 rounded hover:bg-white hover:text-green-500 hover:border-green-500 p-2 text-sm sm:text-base">
+                    Cookies
                 </button>
             </Link>
-        </>
-        
-    )
-}
-export default HomeBtn
+        </div>
+    );
+};
+
+export default HomeBtn;
+
